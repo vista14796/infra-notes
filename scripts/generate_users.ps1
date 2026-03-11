93c78744380b516b21548ad29dac01d070ec3ec3
MengWun Pan <vista14796@gmail.com>
2026-03-11 11:48:17 +0800

    Update generate_users.ps1

--- scripts/generate_users.ps1
+++ scripts/generate_users.ps1
@@ -1,4 +1,5 @@
 #先確定DC建立AD帳號時的格式是否一致
+#台湾人名前（ロマー字）：
 
 # Part 1: Generate CSV File
 $Surnames = "Chen","Lin","Huang","Zhang","Li","Wang","Wu","Liu","Tsai","Yang"
@@ -24,7 +25,36 @@ $UserList | Export-Csv -Path "C:\temp\UserList.csv" -NoTypeInformation
 Write-Host "CSV generated at C:\temp\UserList.csv" -ForegroundColor Cyan
 
 
-#以下為中文：
+
+#台湾人名前（漢字）：
+
+
+# Part 1: Generate CSV File
+$Surnames = "陳","林","黃","張","李","王","吳","劉","蔡","楊"
+$FirstNames = "志明","雅婷","冠宇","淑芬","家豪","怡君","俊宏","欣怡","承恩","詠潔"
+$Departments = @("Sales", "IT")
+
+$UserList = @()
+
+for ($i = 1; $i -le 100; $i++) {
+    $RandomName = ($Surnames | Get-Random) + "_" + ($FirstNames | Get-Random)
+    $EmpID = "EMP" + $i.ToString("000")
+    $Dept = if ($i -le 50) { "Sales" } else { "IT" }
+    
+    $UserList += [PSCustomObject]@{
+        Name       = $RandomName
+        EmployeeID = $EmpID
+        Dept       = $Dept
+        Title      = "Staff"
+    }
+}
+
+$UserList | Export-Csv -Path "C:\temp\UserList.csv" -NoTypeInformation
+Write-Host "CSV generated at C:\temp\UserList.csv" -ForegroundColor Cyan
+
+
+
+#日本人名前：
 
 
 # Part 1: Generate CSV File
